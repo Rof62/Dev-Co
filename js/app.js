7fb24c257ff8a72ffb8fb03414c4919e773a5bf5
rof62 <rof62400@hotmail.com>
2023-06-20 09:10:10 +0200

    dossier js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar")
+
+
+menu.addEventListener('click', (e) => {
+    navbar.classList.toggle("active")
+})
