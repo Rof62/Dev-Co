1e70455c635720a0be92fe583a8823403679571a
Jérémy Lefevre <jeremylefevredev>
2023-06-21 16:17:28 +0200

    feat : MAJ Portfolio & Contact pages

--- js/app.js
+++ js/app.js
@@ -5,3 +5,34 @@ const navbar = document.querySelector(".navbar")
 menu.addEventListener('click', (e) => {
     navbar.classList.toggle("active")
 })
+
+
+// Carrousel
+
+var carousels = document.querySelectorAll('.carousel');
+
+carousels.forEach(function(carousel) {
+var figure = carousel.querySelector('figure'),
+    nav = carousel.querySelector('.carouselBtn'),
+    numImages = figure.childElementCount,
+    theta = 2 * Math.PI / numImages,
+    currImage = 0;
+
+nav.addEventListener('click', onClick, true);
+
+function onClick(e) {
+    e.stopPropagation();
+
+    var t = e.target;
+    if (t.tagName.toUpperCase() != 'BUTTON')
+    return;
+
+    if (t.classList.contains('suivant')) {
+        currImage++;
+    } else {
+        currImage--;
+    }
+
+    figure.style.transform = `rotateY(${currImage * -theta}rad)`;
+}
+});
