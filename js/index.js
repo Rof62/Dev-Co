d9d1d45c2d4073f345ee5f3beaf43c496ec3b7bd
Jérémy Lefevre <jeremylefevredev>
2023-08-02 15:54:12 +0200

    Modal connexion/inscription terminé et responsive ok

--- js/index.js
+++ js/index.js
@@ -10,26 +10,36 @@ menu.addEventListener('click', (e) => {
 
 
 /***************************************************************** MODAL CONNEXION ******************************************************/
-const connexionNav = document.querySelectorAll(".connexion")
+const connexionNav = document.querySelector(".connexion")
 const flipCard = document.querySelector(".flip-card")
 const inscriptionBtn = document.querySelector(".sinscrire")
 const flipCardInner = document.querySelector(".flip-card-inner")
 const connexionBtn = document.querySelector(".connexionBtn")
 const flipCardBack = document.querySelector(".flip-card-back")
 const flipCardFront = document.querySelector(".flip-card-front")
+const connexionResponsive = document.querySelector("#connexionResponsive")
 
 
-for(let button of connexionNav){
-    button.addEventListener("click", () => {
-        if (flipCard.style.visibility === "visible") {
-        flipCard.style.visibility="hidden"
-        } else {
-          flipCard.style.visibility = "visible";
-          flipCard.style.transform="translate(-50%,-50%) scale(1)"
-        }
-      });
-}
+connexionNav.addEventListener("click", () => {
+    if (flipCard.style.visibility === "visible") {
+    flipCard.style.visibility="hidden"
+    flipCard.style.transform="translate(-50%,-50%) scale(0)"
 
+    } else {
+      flipCard.style.visibility = "visible";
+      flipCard.style.transform="translate(-50%,-50%) scale(1)"
+    }
+  });
+
+  connexionResponsive.addEventListener("click", () => {
+    if (flipCard.style.visibility === "visible") {
+    flipCard.style.visibility="hidden"
+    flipCard.style.transform="translate(-50%,-50%) scale(1)"
+    } else {
+      flipCard.style.visibility = "visible";
+      flipCard.style.transform="translate(-50%,-50%) scale(1)"
+    }
+  });
 
     inscriptionBtn.addEventListener("click", () => {
         flipCardInner.style.transform="rotateY(180deg)"
@@ -38,14 +48,16 @@ for(let button of connexionNav){
         flipCardInner.style.transform="rotateY(0)"
     })
 
-    window.addEventListener("click",(e)=>{
-        if(flipCard.style.visibility === "visible" && e.target !== flipCardFront && e.target !== connexionNav){
-            flipCard.style.visibility="hidden"
-            flipCard.style.transform="translate(-50%,-50%) scale(0)"
-            
-        }
-    })
+    
+
 
     flipCard.addEventListener("click", (e) => {
         e.stopPropagation();
       });
+
+
+        window.addEventListener("click",(e)=>{
+        if(flipCard.style.visibility === "visible" && e.target !== flipCardFront && e.target !==connexionNav && e.target !== connexionResponsive ){
+            flipCard.style.visibility="hidden"
+            flipCard.style.transform="translate(-50%,-50%) scale(0)"    
+       }})
